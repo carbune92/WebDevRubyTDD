40aae7cdd0a7e35a9dd6e275ef3ab85634a5517d
Horia <carbune.horia@yahoo.com>
2013-11-08 01:59:01 +0200

    done

--- PascalsTriangle/pascalstriangle.rb
+++ PascalsTriangle/pascalstriangle.rb
@@ -1,4 +1,50 @@
 def compute_row(n)
-  []
-end
+=begin
+  trian = []
+  trian << [1]
+ 
+  if !n.is_a? Integer
+    return nil
+  end  
 
+  if n == 1 
+    return trian[0]
+  end
+  
+  2.upto(n) do |m|
+    row = Array.new(m)
+    row[0] = row[m-1] = 1
+    for i in (1..m-2)
+      row[i] = trian.last[i-1] + trian.last[i]
+    end
+    trian << row
+  end
+  
+  trian[n-1]
+=end
+
+  if !n.is_a? Integer
+    return nil
+  end
+
+  if n == 1
+    return [1]
+  end
+  
+  row = [1]
+
+  2.upto(n) do |m|
+    row.unshift(1)
+    
+    for i in (1..(m-2)/2)
+      row[i]+=row[i+1]
+      row[m-1-i] = row[i]
+    end
+    
+    if (m-2)%2 != 0
+      row[(m-2)/2+1] *= 2         
+    end
+  end
+
+  row
+end
